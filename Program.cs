313673627e59dbaeebe3415049a3de9a16c18698
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-04-05 23:58:39 +0530

    :tada: basics initalization

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CSharpFundamentals.Basics;
+
+namespace CSharpFundamentals
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+      Variables.Run();
+    }
+  }
+}

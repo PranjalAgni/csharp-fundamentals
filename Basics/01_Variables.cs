7b87a8051102d827eefcb66acb0b7b5dcbe3cd18
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-04-06 02:08:22 +0530

    :seedling: fixed variables lesson in csharp

--- Basics/01_Variables.cs
+++ Basics/01_Variables.cs
@@ -16,9 +16,13 @@ namespace CSharpFundamentals.Basics
     {
       int explicitVar = 10; // Explicitly typed
       var implicitVar = 10; // Implicitly typed
+      Console.WriteLine(explicitVar);
+      Console.WriteLine(implicitVar);
 
       var count = 1; // Assign initial value
+      Console.WriteLine("Old value " + count);
       count = 2;     // Update to new value
+      Console.WriteLine("New value " + count);
 
       var calculator = new Calculator();
       var sum = calculator.Add(2, 5);
